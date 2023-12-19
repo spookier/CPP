a564fd2ec9475e4b74411358027351de5c39b3b7
spookier <77325667+spookier@users.noreply.github.com>
2023-12-19 11:27:05 +0100

    module03

---
+++ module-02/ex00/incs/FixedPoint.hpp
@@ -0,0 +1,22 @@
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <iostream>
+
+class Fixed
+{
+	private:
+		int 				fixed_point_value;
+		static const int	fractionalBits = 8;
+
+	public:
+		Fixed(); 
+		Fixed(const Fixed& other); 
+		Fixed& operator=(const Fixed& other); 
+		~Fixed(); // Destructor
+
+		int getRawBits() const;
+		void setRawBits(int const raw);
+};
+
+#endif
