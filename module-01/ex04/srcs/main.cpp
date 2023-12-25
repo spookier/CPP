a2fb9b609eeb46ac9d74810356814877d1393be5
Alex <77325667+spookier@users.noreply.github.com>
2023-12-26 00:20:03 +0100

    fixed replace

--- module-01/ex04/srcs/main.cpp
+++ module-01/ex04/srcs/main.cpp
@@ -4,28 +4,28 @@
 
 void find_and_replace(const std::string& line, const std::string& s1, const std::string& s2, std::string& new_line)
 {
-    size_t startPos;
-	size_t foundPos;
+    size_t startPos = 0; 												// Initial position to start searching from
 
-	startPos = 0;
-    
-	foundPos = line.find(s1, startPos);								// Find the first occurrence of s1
+    while (1)
+	{
+        size_t foundPos = line.find(s1, startPos); 						// foundPos = find the index of the word to replace starting from startPos
 
-	if (foundPos != std::string::npos)								// If we found s1 in the string
-	{
-		new_line.append(line, startPos, foundPos - startPos);		// Append everything from startPos to foundPos
-		new_line.append(s2);
-		startPos = foundPos + s1.length();
-	}
-	else															// If we didn't find s1 in the string
-	{
-		new_line.append(line, startPos);							// Append everything
-	}
+        if (foundPos != std::string::npos)
+		{
+            new_line += line.substr(startPos, foundPos - startPos) + s2;
+            startPos = foundPos + s1.length(); 							// Change startPos value to the end of last occurence
+        }
+		else
+		{
+            new_line += line.substr(startPos);
+            break;
+        }
+    }
 }
 
-int check_args(std::string program_name, std::string s1, std::string s2)
+int check_args(std::string &program_name)
 {
-	if (program_name.empty() || s1.empty() || s2.empty())
+	if (program_name.empty())
 	{
 		std::cout << "Invalid arguments!\n" << std::endl;
 		return(1);
@@ -41,8 +41,8 @@ int main(int argc, char **argv)
 	std::string 	line;
 	std::string		new_filename;
 	std::string		new_line;
-	std::ifstream	file;												// Only reading permissions
-	std::ofstream	new_file;											// Only writing permissions
+	std::ifstream	file;											// Only reading permissions
+	std::ofstream	new_file;										// Only writing permissions
 
 	if (argc != 4)
 	{
@@ -53,26 +53,32 @@ int main(int argc, char **argv)
 	filename		= argv[1];
 	s1				= argv[2];
 	s2				= argv[3];
-	if(check_args(filename, s1, s2) == 1)
+
+	if (s1.empty())
+	{
+		std::cout << "s1 should not be empty" << std::endl;
+		return(1);
+	}
+	if (check_args(filename) == 1)
 		return(1);
 
 	new_filename = filename + ".replace";
-	
+
 	new_file.open(new_filename.c_str());							// Create the new file 	
-	if(!new_file.is_open())
+	if (!new_file.is_open())
 	{
 		std::cout << "File failed to create" << std::endl;
 		return(1);
 	}
 
-	file.open(filename.c_str(), std::ios::in | std::ios::out);
-	if(file.is_open())
+	file.open(filename.c_str(), std::ios::in);						// Open for reading
+	if (file.is_open())
 	{
 		while (std::getline(file, line))
 		{
 			new_line.clear();
 			find_and_replace(line, s1, s2, new_line);
-			new_file << new_line << std::endl;
+			new_file << new_line << std::endl;      
 		}
 		file.close();
 		new_file.close();
